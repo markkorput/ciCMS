82a8ddea91b38e0e24eb5e6dc0671d4fa4c81061
Mark van de Korput <mark.korput@gmail.com>
2017-06-01 14:20:28 +0200

    ModelBase implementation copied from ofxCMS OF addon

---
+++ src/ciCMS/ModelBase.cpp
@@ -0,0 +1,94 @@
+#include "ModelBase.h"
+#include "cinder/Log.h"
+
+using namespace cms;
+
+ModelBase* ModelBase::set(const string &attr, const string &value, bool notify){
+    if(isLocked()){
+        CI_LOG_V("model locked; queueing .set operation for attribute: " << attr);
+        modQueueRefs.push_back(make_shared<Mod>(attr, value, notify));
+        return this;
+    }
+
+    string old_value = _attributes[attr];
+
+    _attributes[attr] = value;
+    onSetAttribute(attr, value);
+
+    if(notify && old_value != value){
+        AttrChangeArgs args;
+        args.model = this;
+        args.attr = attr;
+        args.value = value;
+        onAttributeChanged(attr, value, old_value);
+
+        if(notify){
+            changeSignal.emit(*this);
+            attributeChangeSignal.emit(args);
+        }
+    }
+
+    // returning `this` allows the caller to link operations, like so:
+    // model.set('name', 'Johnny')->set('surname', 'Blaze')->set('age', '44');
+    return this;
+}
+
+
+ModelBase* ModelBase::set(map<string, string> &attrs, bool notify){
+    for(map<string, string>::iterator it=attrs.begin(); it != attrs.end(); it++){
+        this->set(it->first, it->second, notify);
+    }
+
+	return this;
+}
+
+string ModelBase::get(const string &attr, string _default) const {
+    return has(attr) ? _attributes.at(attr) : _default;
+}
+
+bool ModelBase::has(const string& attr) const {
+    return (_attributes.find(attr) == _attributes.end()) ? false : true;
+}
+
+void ModelBase::each(AttrIterateFunc func){
+    lock([this, &func](){
+        for(auto pair : this->_attributes){
+            func(pair.first, pair.second);
+        }
+    });
+}
+
+void ModelBase::copy(shared_ptr<ModelBase> otherRef, bool also_ids){
+    if(!otherRef){
+        CI_LOG_W("ofxCMS::ModelBase.copy got nullptr");
+        return;
+    }
+
+    copy(*otherRef.get(), also_ids);
+}
+
+void ModelBase::copy(ModelBase& other, bool also_ids){
+    other.each([this, also_ids](const string& key, const string& value){
+        if((key != "id" && key != "_id") || also_ids)
+            this->set(key, value);
+    });
+}
+
+void ModelBase::lock(LockFunctor func){
+    lockCount++;
+    func();
+    lockCount--;
+
+    // still (recursively) locked? skip processing of opereations queue
+    if(isLocked())
+        return;
+
+    // after we're done iterating, we should process any items
+    // accumulated in the vector modificaton queue
+    for(auto modRef : modQueueRefs){
+        set(modRef->attr, modRef->value, modRef->notify);
+        // TODO; add support for removing attributes?
+    }
+
+    modQueueRefs.clear();
+}
--- src/ciCMS/ModelBase.h
+++ src/ciCMS/ModelBase.h
@@ -1,10 +1,78 @@
 #include <memory>
+#include <functional>
+#include "cinder/app/App.h"
 
 using namespace std;
 
 namespace cms {
     class ModelBase {
-    private:
-        
+
+        typedef void* CidType;
+
+        typedef function<void(const string&, const string&)> AttrIterateFunc;
+        typedef function<void(void)> LockFunctor;
+
+        // used in attributeChangeEvent notifications
+        class AttrChangeArgs {
+        public:
+            ModelBase *model;
+            string attr;
+            string value;
+        };
+
+        //! Sub-type used a record-format when queueing modifications while to model is locked
+        class Mod {
+            public:
+                string attr, value;
+                bool notify;
+                Mod(const string& _attr, const string& _value, bool _notify=true) : attr(_attr), value(_value), notify(_notify){}
+        };
+
+    public:
+
+        ModelBase() : lockCount(0){}
+        ModelBase* set(const string &attr, const string &value, bool notify = true);
+        ModelBase* set(map<string, string> &attrs, bool notify=true);
+        string get(const string &attr, string _default = "") const;
+
+        string getId() const { return get("id", get("_id")); }
+        CidType cid() const { return (CidType)this; }
+
+        const map<string, string> &attributes() const { return _attributes; }
+
+        bool has(const string& attr) const;
+        bool equals(shared_ptr<ModelBase> other){ return other->cid() == cid(); }
+        size_t size() const { return _attributes.size(); }
+
+        void each(AttrIterateFunc func);
+        void copy(shared_ptr<ModelBase> otherRef, bool also_ids=false);
+        void copy(ModelBase& other, bool also_ids=false);
+
+    protected: // methods
+
+        bool isLocked() const { return lockCount > 0; }
+        void lock(LockFunctor func);
+
+    protected: // callbacks
+
+        //! this virtual method is called whenever an attribute is written (using this->set()) and can be overwritten be inheriting classes
+        virtual void onSetAttribute(const string &attr, const string &value){}
+        //! this virtual method is called whenever an attribute is changed (using this->set()) and can be overwritten be inheriting classes
+        virtual void onAttributeChanged(const string &attr, const string &value, const string &old_value){}
+
+    public: // events
+
+        //! this event is triggered whenever the model changes (which means; when any attribute changes) and gives the caller a reference to this model
+        ci::signals::Signal<void(const ModelBase&)> changeSignal;
+        //! this event is triggered whenever the model changes (which means; when any attribute changes) and gives the caller an object with a pointer to the model and information about which attribute changed
+        ci::signals::Signal<void(const AttrChangeArgs&)> attributeChangeSignal;
+
+    private: // attributes
+
+        //! the internal storage map for this model's attributes
+        map<string, string> _attributes;
+        // a counter to track the number of active (recursive) locks
+        int lockCount;
+        std::vector<shared_ptr<Mod>> modQueueRefs;
     };
 }
