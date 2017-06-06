a97311d556e795f0f8f287ed9dcedcaa4969aac9
Mark van de Korput <mark.korput@gmail.com>
2017-06-06 16:50:52 +0200

    Moved ModelCollection specialization implementations into cpp file

--- src/ciCMS/ModelCollection.h
+++ src/ciCMS/ModelCollection.h
@@ -10,33 +10,12 @@ namespace cms {
     };
 }
 
+// Specialization implementations can be found in ModelCollection.cpp (obviously)
 template<>
-shared_ptr<cms::Model> cms::CollectionJsonLoader<cms::Model>::findMatch(ci::JsonTree& jsonTree, CollectionBase<Model>& collection){
-    if(!jsonTree.hasChild("id"))
-        return nullptr;
-
-    std::string id = jsonTree.getValueForKey("id");
-
-    return collection.first([&id](shared_ptr<cms::Model> modelRef){
-        return modelRef->get("id") == id;
-    });
-}
+shared_ptr<cms::Model> cms::CollectionJsonLoader<cms::Model>::findMatch(ci::JsonTree& jsonTree, CollectionBase<Model>& collection);
 
 template<>
-bool cms::CollectionJsonLoader<cms::Model>::loadItem(ci::JsonTree& jsonTree, shared_ptr<Model> itemRef){
-    for(int idx=0; idx<jsonTree.getNumChildren(); idx++){
-        ci::JsonTree subTree = jsonTree.getChild(idx);
-        itemRef->set(subTree.getKey(), subTree.getValue());
-    }
-    return true;
-}
+bool cms::CollectionJsonLoader<cms::Model>::loadItem(ci::JsonTree& jsonTree, shared_ptr<Model> itemRef);
 
 template<>
-ci::JsonTree cms::CollectionJsonWriter<cms::Model>::getItemJsonTree(shared_ptr<cms::Model> itemRef){
-    ci::JsonTree tree;
-    itemRef->each([&](const string& attr, const string& value){
-        tree.addChild(ci::JsonTree(attr, value));
-    });
-
-    return tree;
-}
+ci::JsonTree cms::CollectionJsonWriter<cms::Model>::getItemJsonTree(shared_ptr<cms::Model> itemRef);
