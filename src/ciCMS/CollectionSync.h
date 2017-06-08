346eb044db103efddecda6e0bd32e83b76d71665
Mark van de Korput <mark.korput@gmail.com>
2017-06-08 12:47:45 +0200

    Collection.sync and .stopSync support non shared_ptr collections

--- src/ciCMS/CollectionSync.h
+++ src/ciCMS/CollectionSync.h
@@ -9,52 +9,60 @@ namespace cms {
     class CollectionSync : public CollectionBase<ItemType> {
         public: // methods
 
-            CollectionSync() : target(NULL), source(nullptr){}
+            CollectionSync() : target(NULL), pSource(NULL), sourceRef(nullptr){}
             ~CollectionSync(){ destroy(); }
 
+            void setup(CollectionBase<ItemType> *target, CollectionBase<ItemType>& source);
             void setup(CollectionBase<ItemType> *target, shared_ptr<CollectionBase<ItemType>> source);
             void destroy();
 
-            shared_ptr<CollectionBase<ItemType>> getSource() const { return source; }
+            CollectionBase<ItemType>& getSource() const { return sourceRef ? sourceRef.get() : pSource; }
 
         private: // attributes
 
             CollectionBase<ItemType> *target;
-            shared_ptr<CollectionBase<ItemType>> source;
+            CollectionBase<ItemType>* pSource;
+            shared_ptr<CollectionBase<ItemType>> sourceRef;
             std::vector<ci::signals::Connection> signalConnections;
     };
 }
 
 template<class ItemType>
 void cms::CollectionSync<ItemType>::setup(CollectionBase<ItemType> *target, shared_ptr<CollectionBase<ItemType>> source){
+    sourceRef = source;
+    setup(target, *sourceRef.get());
+}
+
+template<class ItemType>
+void cms::CollectionSync<ItemType>::setup(CollectionBase<ItemType> *target, CollectionBase<ItemType>& source){
     destroy();
 
     this->target = target;
-    this->source = source;
+    this->pSource = &source;
 
-    if(!source){
-        CI_LOG_W("got nullptr source to sync from");
+    if(!pSource){
+        CI_LOG_W("got NULL source to sync from");
         return;
     }
 
     // add all instances currently in source
-    source->each([this](shared_ptr<ItemType> instanceRef){
+    pSource->each([this](shared_ptr<ItemType> instanceRef){
         if(!this->target->has(instanceRef)){
             this->target->add(instanceRef);
         }
     });
 
-    // actively monitor for new instances added to source
-    auto conn = source->addSignal.connect([&](ItemType& instance){
+    // actively monitor for new instances added to pSource
+    auto conn = pSource->addSignal.connect([&](ItemType& instance){
         if(!this->target->has(&instance)){
-            this->target->add(this->source->find(&instance)); // need to convert ref var to shared_ptr
+            this->target->add(this->pSource->find(&instance)); // need to convert ref var to shared_ptr
         }
     });
 
     signalConnections.push_back(conn);
 
-    // actively montor for instances removed from source
-    conn = source->removeSignal.connect([this](ItemType& instance){
+    // actively montor for instances removed from pSource
+    conn = pSource->removeSignal.connect([this](ItemType& instance){
         if(this->target->has(&instance))
             this->target->remove(&instance);
     });
@@ -69,5 +77,6 @@ void cms::CollectionSync<ItemType>::destroy(){
     }
 
     signalConnections.clear();
-    source = nullptr;
+    pSource = NULL;
+    sourceRef = nullptr;
 }
