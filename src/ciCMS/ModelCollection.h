5ecd45b9541165f3c4baf37871268aae48adcf8f
mark <mark.korput@gmail.com>
2018-02-07 23:29:09 +0100

    windows-only changes

--- src/ciCMS/ModelCollection.h
+++ src/ciCMS/ModelCollection.h
@@ -27,54 +27,57 @@ namespace cms {
 namespace cms {
 #ifndef CINDER_MSW
 
-	// non-windows; implementations in ModelCollection.cpp
+    // non-windows; implementations in ModelCollection.cpp
 
-	template<>
-	shared_ptr<Model> CollectionJsonLoader<Model>::findMatch(ci::JsonTree& jsonTree, CollectionBase<Model>& collection);
+    template<>
+    shared_ptr<Model> CollectionJsonLoader<Model>::findMatch(ci::JsonTree& jsonTree, CollectionBase<Model>& collection);
 
-	template<>
-	bool CollectionJsonLoader<Model>::loadItem(ci::JsonTree& jsonTree, shared_ptr<Model> itemRef);
+    template<>
+    bool CollectionJsonLoader<Model>::loadItem(ci::JsonTree& jsonTree, shared_ptr<Model> itemRef);
 
-	template<>
-	ci::JsonTree CollectionJsonWriter<Model>::getItemJsonTree(shared_ptr<Model> itemRef);
+    template<>
+    ci::JsonTree CollectionJsonWriter<Model>::getItemJsonTree(shared_ptr<Model> itemRef);
 
 #else
-	template<>
-	shared_ptr<Model> CollectionJsonLoader<Model>::findMatch(ci::JsonTree& jsonTree, CollectionBase<Model>& collection){
-        auto id = jsonTree.hasChild("id") ? jsonTree.getValueForKey("id") : jsonTree.getKey();
+    template<>
+    shared_ptr<Model> CollectionJsonLoader<Model>::findMatch(ci::JsonTree& jsonTree, CollectionBase<Model>& collection){
+    std::string id;
 
-		// if (!jsonTree.hasChild("id"))
-		// 	return nullptr;
+    if(jsonTree.hasChild("id")) {
+      id = jsonTree.getValueForKey("id");
+    } else if (jsonTree.hasParent() && jsonTree.getParent().getNodeType() == ci::JsonTree::NODE_OBJECT) {
+      id = jsonTree.getKey();
+    } else {
+      return nullptr;
+    }
 
-		// std::string id = jsonTree.getValueForKey("id");
-
-		return collection.first([&id](shared_ptr<Model> modelRef) {
-			return modelRef->get("id") == id;
-		});
-	}
+    return collection.first([&id](shared_ptr<Model> modelRef) {
+      return modelRef->get("id") == id;
+    });
+    }
 
     template<>
     bool CollectionJsonLoader<Model>::loadItem(ci::JsonTree& jsonTree, shared_ptr<Model> itemRef){
-        for (int idx = 0; idx < jsonTree.getNumChildren(); idx++) {
-            ci::JsonTree subTree = jsonTree.getChild(idx);
-            itemRef->set(subTree.getKey(), subTree.getValue());
-        }
+      for (int idx = 0; idx < jsonTree.getNumChildren(); idx++) {
+        ci::JsonTree subTree = jsonTree.getChild(idx);
+        itemRef->set(subTree.getKey(), subTree.getValue());
+      }
 
-        if (!jsonTree.hasChild("id")) {
-            itemRef->set("id", jsonTree.getKey());
-        }
+      if (!jsonTree.hasChild("id")) {
+          itemRef->set("id", jsonTree.getKey());
+      }
 
-        return true;
+      return true;
     }
 
     template<>
     ci::JsonTree CollectionJsonWriter<Model>::getItemJsonTree(shared_ptr<Model> itemRef){
-    	ci::JsonTree tree;
-    	itemRef->each([&](const string& attr, const string& value) {
-    		tree.addChild(ci::JsonTree(attr, value));
-    	});
+        ci::JsonTree tree;
+        itemRef->each([&](const string& attr, const string& value) {
+            tree.addChild(ci::JsonTree(attr, value));
+        });
 
-    	return tree;
+        return tree;
     }
 #endif
 }
