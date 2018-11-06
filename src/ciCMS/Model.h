32c12d7bb90dde019c20d7d20311826aee1ddc2d
mark <mark.korput@gmail.com>
2018-11-06 13:53:53 +0100

    make Model.transform return-by-value

--- src/ciCMS/Model.h
+++ src/ciCMS/Model.h
@@ -10,22 +10,22 @@ namespace cms{
 
 
             //! registers an attribute transformer
-            ci::signals::Connection& transformAttribute(const string& attr, AttributeTransformFunctor func, bool active=true){
+            ci::signals::Connection transformAttribute(const string& attr, AttributeTransformFunctor func, bool active=true){
                 return transformAttribute(attr, func, NULL, active);
             }
 
-            ci::signals::Connection& transformAttribute(const string& attr, AttributeTransformFunctor func, void* owner, bool active=true){
+            ci::signals::Connection transformAttribute(const string& attr, AttributeTransformFunctor func, void* owner, bool active=true){
                 std::vector<string> attrs;
                 attrs.push_back(attr);
                 return transformAttributes(attrs, func, owner, active);
             }
 
             //! register the same transformer on various attributes
-            ci::signals::Connection& transformAttributes(const std::vector<string> &attrs, AttributeTransformFunctor func, bool active=true){
+            ci::signals::Connection transformAttributes(const std::vector<string> &attrs, AttributeTransformFunctor func, bool active=true){
                 return transformAttributes(attrs, func, NULL, active);
             }
 
-            ci::signals::Connection& transformAttributes(const std::vector<string> &attrs, AttributeTransformFunctor func, void* owner, bool active=true){
+            ci::signals::Connection transformAttributes(const std::vector<string> &attrs, AttributeTransformFunctor func, void* owner, bool active=true){
                 auto connection = this->attributeChangeSignal.connect([func, attrs](const Model::AttrChangeArgs& args){
                     // see if we "have" the changed attribute
                     bool have_attribute = false;
@@ -65,7 +65,7 @@ namespace cms{
             }
 
 
-            ci::signals::Connection& transform(ModelTransformFunctor func, void* owner = NULL, bool active=true){
+            ci::signals::Connection transform(ModelTransformFunctor func, void* owner = NULL, bool active=true){
                 // register change listener to invoke the functor
                 auto connection = this->changeSignal.connect(func);
 
@@ -77,7 +77,7 @@ namespace cms{
                 else
                     connection.disable();
 
-				return connection;
+                return connection;
             }
 
             std::vector<ci::signals::Connection>& stopTransform(void* owner){
