865963fd25caae03eaf8a72e9960464711134dfa
mark <mark.korput@gmail.com>
2020-10-18 12:46:50 +0200

    Base takes name property and uses it for logging, script does more logging

--- src/ciCMS/cfg/components/Base.h
+++ src/ciCMS/cfg/components/Base.h
@@ -8,14 +8,17 @@ namespace cms { namespace cfg { namespace components {
 
     public:
       inline void cfg(cms::cfg::Cfg& cfg) {
-        cfg.setBool("verbose", this->bVerbose);
+        cfg
+        .setBool("verbose", this->bVerbose)
+        .set("name", this->name);
       }
 
       inline void verbose(const std::string& msg) const {
-        if (bVerbose) CI_LOG_I(msg);
+        if (bVerbose) CI_LOG_I("["+name+"] "+msg);
       }
 
-    protected:
+    private:
       bool bVerbose = false;
+      std::string name = "NoName";
   };
 }}}
--- src/ciCMS/cfg/components/Script.cpp
+++ src/ciCMS/cfg/components/Script.cpp
@@ -8,11 +8,12 @@ void Script::cfg(cms::cfg::Cfg& cfg){
 
   cfg
   .connectAttr<void()>("on", [this](){
-    this->verbose("Script::run");
+    this->verbose("Script::cfg::on");
     if (this->func) this->func();
     else this->verbose("Script::run NO FUNC");  
   })
   .with("do", [this, &cfg](const std::string& script){
+    this->verbose("Script::cfg::do");
     this->func = cfg.compileScript(script);
   })
   .reader();
